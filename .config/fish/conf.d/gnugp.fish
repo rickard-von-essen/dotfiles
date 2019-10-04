39036a214204fef37f4600fd5823b82b2d0f06bb
Rickard von Essen <rickard.von.essen@izettle.com>
2019-10-04 11:03:19 +0200

    New GPG key

--- .config/fish/conf.d/gnugp.fish
+++ .config/fish/conf.d/gnugp.fish
@@ -1,4 +1,5 @@
-set -x KEYID 0xB7DB909C0672FC49
+#set -x KEYID 0xB7DB909C0672FC49
+set -x KEYID 0x7C8F9BC8B2E6BB2A
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
