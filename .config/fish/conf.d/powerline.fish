1af7b02fbfe1a80b730e38ca25dd527e3d4d7486
Rickard von Essen <rickard.von.essen@izettle.com>
2020-05-04 11:08:15 +0200

    Updated Powerline paths

--- .config/fish/conf.d/powerline.fish
+++ .config/fish/conf.d/powerline.fish
@@ -1,6 +1,9 @@
 test -d /usr/share/powerline/fish; and \
   set fish_function_path $fish_function_path /usr/share/powerline/fish
 
+test -d /usr/share/powerline/bindings/fish; and \
+  set fish_function_path $fish_function_path /usr/share/powerline/bindings/fish
+
 test -d "$HOME/src/powerline/powerline/bindings/fish"; and \
   set fish_function_path $fish_function_path "$HOME/src/powerline/powerline/bindings/fish"
 
