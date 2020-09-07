70eaaaf9acdb465daa7ecf2f1b15d4d33c08e4d6
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-07 14:03:21 +0200

    Removing Python 3.7 ref

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -1,2 +1,3 @@
 # The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
 if [ -f "$HOME/local/google-cloud-sdk/path.fish.inc" ]; . "$HOME/local/google-cloud-sdk/path.fish.inc"; end
