9c64f1b5c66450cf821d4091553bf757094c5522
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-10 10:12:40 +0200

    Fish: source autojump config also in system install

--- .config/fish/conf.d/autojump.fish
+++ .config/fish/conf.d/autojump.fish
@@ -1,1 +1,4 @@
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
