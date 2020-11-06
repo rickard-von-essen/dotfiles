71a89a511e517e55a4e723abde6574eda57d8fca
Rickard von Essen <rickard.von.essen@gmail.com>
2020-11-06 10:21:30 +0100

    Updated fish plugins

--- .config/fish/conf.d/autojump.fish
+++
@@ -1,4 +0,0 @@
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
-
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
