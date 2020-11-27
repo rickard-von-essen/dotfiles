837d5238bc50cb12677870e8d8b5e60086d621ae
Rickard von Essen <rickard.von.essen@gmail.com>
2020-11-27 09:48:19 +0100

    Bunch of updates

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -1,3 +1,6 @@
+# Gcloud don't work well with Python 3.9 yet
+set -x CLOUDSDK_PYTHON /usr/bin/python3
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
 if [ -f "$HOME/local/google-cloud-sdk/path.fish.inc" ]; . "$HOME/local/google-cloud-sdk/path.fish.inc"; end
