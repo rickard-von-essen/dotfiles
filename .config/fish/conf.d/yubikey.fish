e6d530289ec739f34d0c81aa54af2b59d3d757b9
Rickard von Essen <rickard.von.essen@izettle.com>
2021-03-18 16:23:17 +0100

    Ykman: updated to non deprecated command

--- .config/fish/conf.d/yubikey.fish
+++ .config/fish/conf.d/yubikey.fish
@@ -6,6 +6,6 @@ function totp -a command -a filter -d "Get/List totp from Yubikey"
     ykman -d $DEVICE_ID oath list
 
   else if [ $command = "get" ]
-    ykman -d $DEVICE_ID oath code -s $filter | pbcopy
+    ykman -d $DEVICE_ID oath accounts code -s $filter | pbcopy
   end
 end
