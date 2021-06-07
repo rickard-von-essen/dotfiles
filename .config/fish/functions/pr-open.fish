52de9db72e305ee1916afff7b735bbb50bb2964a
Rickard von Essen <ext.rickard.von@king.com>
2021-06-07 09:24:06 +0200

    Reimplemented pr-open

--- .config/fish/functions/pr-open.fish
+++ .config/fish/functions/pr-open.fish
@@ -1,25 +1,7 @@
-function pr-open --description "List open PR's and open them in your browser"
+# Reuires gh (GitHub CLI), peco, and sed
+function pr-open -a pr -d "Open a PR in the browser"
+  test -z "$pr" && set -l pr (gh pr list | peco | sed -ne 's/^\([0-9]*\).*/\1/p')
 
-  argparse 'h/help' 'c/current' -- $argv
-
-  if set -q _flag_help
-    echo 'pr-open       Open a PR in the browser
-              If no flag is given a list of open PRs will be shown to choose from.
-
- -c --current      Open the current branchs PR
- -h --help         Show this help'
-    return
-  end
-
-  if set -q _flag_current
-    set PR_REF (git branch --show-current)
-  else
-    set PR_LINE (gh pr list | peco)
-    if test -z "$PR_LINE"
-      return
-    end
-    set PR_REF (echo "$PR_LINE" | cut -d\t -f1)
-  end
-
-  gh pr view $PR_REF
+  test -z "$pr" || gh pr view -w $pr
 end
+
