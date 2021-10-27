66e40f6565d11bb02fd9124242692616c6944431
Rickard von Essen <rickard.von.essen@gmail.com>
2021-10-27 21:20:06 +0200

    Alacritty and hyper updates

--- .config/hyper/.hyper.js
+++ .config/hyper/.hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 10,
 
     // font family with optional fallbacks
     fontFamily: '"Hasklug Nerd Font Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -59,12 +59,12 @@ module.exports = {
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: 'false',
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: 'false',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: '12px 14px',
