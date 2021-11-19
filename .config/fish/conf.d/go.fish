e1cebd0d0c48abd8530e1da0aed17cd3edf00714
Rickard von Essen <rickard.von.essen@gmail.com>
2021-11-19 09:02:30 +0100

    Fish: use fish_add_path to set path

--- .config/fish/conf.d/go.fish
+++ .config/fish/conf.d/go.fish
@@ -1,3 +1,3 @@
-test -d $HOME/src/go-code/bin; and set -gx fish_user_path $fish_user_path $HOME/src/go-code/bin
+test -d $HOME/src/go-code/bin; and fish_add_path $HOME/src/go-code/bin
 test -d $HOME/src/go-code; and set -gx GOPATH $HOME/src/go-code
-test -d $HOME/go/bin; and set -gx fish_user_path $HOME/go/bin $fish_user_path
+test -d $HOME/go/bin; and fish_add_path $HOME/go/bin
