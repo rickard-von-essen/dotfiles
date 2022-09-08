4e300f2a4a9b85cad668005316fade06d37b72ea
Rickard von Essen <rickard.von.essen@gmail.com>
2022-09-08 09:12:09 +0200

    Removed functions for swithing Java version, use asdf instead

--- .config/fish/functions/java8.fish
+++
@@ -1,6 +0,0 @@
-function java8 -d "Use JDK 8"
-  set -xg JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
-  set -xg fish_user_paths $JAVA_HOME/bin $fish_user_paths
-  set -xg PATH $JAVA_HOME/bin $PATH
-end
-
