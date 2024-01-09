381f6b5a2edbce6b166e6fab0f89ccfa70d8330c
Rickard von Essen <ext.rickard.von@king.com>
2024-01-09 10:35:41 +0100

    gcloud: Now supports python 3.10

--- .config/fish/conf.d/gcloud.fish
+++ .config/fish/conf.d/gcloud.fish
@@ -2,9 +2,6 @@
 # https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
 set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
 
-# Gcloud don't work well with Python 3.10 yet
-set -x CLOUDSDK_PYTHON (asdf which python)
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
 if [ -f "$HOME/local/google-cloud-sdk/path.fish.inc" ]; . "$HOME/local/google-cloud-sdk/path.fish.inc"; end
