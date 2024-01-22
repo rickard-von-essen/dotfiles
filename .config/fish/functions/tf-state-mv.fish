76d5e1668907d572ac79ed372a76e76211b2b2a2
Rickard von Essen <rickard.von.essen@gmail.com>
2024-01-22 09:52:18 +0100

    fish: tf-state-mv allow adding TF opts

--- .config/fish/functions/tf-state-mv.fish
+++ .config/fish/functions/tf-state-mv.fish
@@ -1,6 +1,6 @@
-function tf-state-mv -a sed_expr -d "Reads stdin with resources that will be moved to <sed_expr>"
+function tf-state-mv -a sed_expr -a tf_opts -d "Reads stdin with resources that will be moved to <sed_expr>"
 
   while read -L line
-    terraform state mv $line (echo "$line" | sed -e "$sed_expr")
+    terraform state mv (string split " " -- "$tf_opts") $line (echo "$line" | sed -e "$sed_expr")
   end
 end
